8583982b0466864351be87475bffba974e26e7d7
mounacj07 <mounacj.07@gmail.com>
2026-08-17 18:47:55 +0530

    Password generator

---
+++ passwordgen.py
@@ -0,0 +1,26 @@
+import random
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+random_letters=random.sample(letters, nr_letters)
+random_symbols=random.sample(symbols, nr_symbols)
+random_numbers=random.sample(numbers, nr_numbers)
+
+list1=[]
+list1.extend(random_letters)
+list1.extend(random_symbols)
+list1.extend(random_numbers)
+print(list1)
+
+list2=random.sample(list1, len(list1))
+print(list2)
+
+password=''.join(list2)
+print(f"Your password is: {password}")
